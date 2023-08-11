0097f88d3945f5b2af940747c22148528d1278c3
Kim Joohee <syap1170@gmail.com>
2023-08-11 14:53:45 +0900

    feat: Add 보드 조회시 데이터 캐싱
    해당 보드에 대한 권한 확인 위해 데이터 캐싱

---
+++ cache.js
@@ -0,0 +1,34 @@
+const nodeCache = require('node-cache');
+const { BoardGroup } = require('./models');
+
+const cache = new nodeCache({ stdTTL: 0, checkperiod: 600 });
+
+class CollaboratorCaching {
+  setCachedCollaborators = async (boardId) => {
+    try {
+      const collaborators = await BoardGroup.findAll({
+        where: { boardId },
+      });
+      for (const collaborator of collaborators) {
+        cache.set(`cacheKey${collaborator.collaborator}`, collaborator);
+      }
+      console.log('collaborator 데이터 캐싱 성공');
+    } catch (err) {
+      console.log('collaborator 데이터 캐싱 실패', err);
+      throw err;
+    }
+  };
+
+  getCachedCollaborator = (collaborator) => {
+    try {
+      const cachedCollaborator = cache.get(`cacheKey${collaborator}`);
+      console.log('collaborator 데이터 GET 성공');
+      return cachedCollaborator;
+    } catch (err) {
+      console.log('collaborator 데이터 GET 실패', err);
+      throw err;
+    }
+  };
+}
+
+module.exports = CollaboratorCaching;
