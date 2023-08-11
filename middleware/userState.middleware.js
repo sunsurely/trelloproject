e053d25a36fefc00f650bd891b7d2181b64ef4cd
Dong Hyeon Oh <dev0doraemon@gmail.com>
2023-08-12 01:34:42 +0900

    feat: 미들웨어 기능 수정 - 하나의 고정된 권한 뿐만 아니라 여러개의 권한을 넣어도 되도록 처리

--- middleware/userState.middleware.js
+++ middleware/userState.middleware.js
@@ -53,37 +53,102 @@ exports.isLoggedIn = async (req, res, next) => {
     next();
   }
 };
+// ("url", auth, isInvitedByPermission, apiFunction)
+// exports.isInvitedByPermission = (permission) => {
+//   const isInvited = async (req, res, next) => {
+//     const userId = res.locals.userId;
+//     try {
+//       if (isNaN(userId) || userId < 1) {
+//         return {
+//           status: 400,
+//           sucess: false,
+//           message: '초대된 유저만 사용가능합니다.',
+//         };
+//       }
 
-exports.isInvitedByPermission = (permission) => {
-  const isInvited = async (req, res, next) => {
+//       const invited = await collaboratorCaching.getCachedCollaborator(userId);
+//       if (!invited) {
+//         return {
+//           status: 400,
+//           sucess: false,
+//           message: '초대된 유저만 사용가능합니다.',
+//         };
+//       }
+
+//       const permission = invited.permission;
+//       if (permission === `owner`) {
+//         next();
+//       }
+//     } catch (err) {
+//       console.log(error);
+//       res.status(400).json({ errorMessage: '잘못된 접근입니다.' });
+//     }
+//   };
+
+//   return isInvited;
+// };
+
+// permission에 표기된 접근권한을 가진 사용자만 next()
+// 입력 예시: isInvitedByPermission("owner", "write", "readonly")
+// 미들웨어 이름 availablePermission 어떤가요?
+exports.isInvitedByPermission = (...permissions) => {
+  return async (req, res, next) => {
     const userId = res.locals.userId;
+    if (isNaN(userId) || userId < 1) {
+      return res
+        .status(400)
+        .json({ message: '초대된 유저만 사용 가능합니다.' });
+    }
+
     try {
-      if (isNaN(userId) || userId < 1) {
-        return {
-          status: 400,
-          sucess: false,
-          message: '초대된 유저만 사용가능합니다.',
-        };
-      }
-
-      const invited = await collaboratorCaching.getCachedCollaborator(userId);
+      const invited = await collaboratorCaching.getChachedCollaborator(userId);
       if (!invited) {
-        return {
-          status: 400,
-          sucess: false,
-          message: '초대된 유저만 사용가능합니다.',
-        };
+        return res
+          .status(400)
+          .json({ message: '초대된 유저만 사용 가능합니다.' });
       }
 
-      const permission = invited.permission;
-      if (permission === `owner`) {
-        next();
+      for (permission of permissions) {
+        if (invited.permission !== permission) {
+          return res.status(400).json({ message: '접근 권한이 없습니다.' });
+        }
       }
     } catch (err) {
-      console.log(error);
-      res.status(400).json({ errorMessage: '잘못된 접근입니다.' });
+      console.error(`Error path: ${__filename}`);
+      console.error(err);
+
+      return res.status(500).json({ message: 'Internal Server Error ' });
     }
   };
+  // const isInvited = async (req, res, next) => {
+  //   const userId = res.locals.userId;
+  //   try {
+  //     if (isNaN(userId) || userId < 1) {
+  //       return {
+  //         status: 400,
+  //         sucess: false,
+  //         message: '초대된 유저만 사용가능합니다.',
+  //       };
+  //     }
 
-  return isInvited;
+  //     const invited = await collaboratorCaching.getCachedCollaborator(userId);
+  //     if (!invited) {
+  //       return {
+  //         status: 400,
+  //         sucess: false,
+  //         message: '초대된 유저만 사용가능합니다.',
+  //       };
+  //     }
+
+  //     const permission = invited.permission;
+  //     if (permission === `owner`) {
+  //       next();
+  //     }
+  //   } catch (err) {
+  //     console.log(error);
+  //     res.status(400).json({ errorMessage: '잘못된 접근입니다.' });
+  //   }
+  // };
+
+  // return isInvited;
 };
