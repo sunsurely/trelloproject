2d8d11057216f8c5079a3aac8dbca9daab38e6ae
Dong Hyeon Oh <dev0doraemon@gmail.com>
2023-08-11 17:41:28 +0900

    feat: 보드 라우터 수정 - 추가 및 수정

--- routes/board.router.js
+++ routes/board.router.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const BoardController = require('../controllers/board.controller');
-const { isLoggedIn } = require('../middlewares/userState.middleware'); // s 붙일지 말지 정하기
+const {
+  isLoggedIn,
+  authorizated,
+} = require('../middleware/userState.middleware'); // s 붙일지 말지 정하기
 
 const boardController = new BoardController();
 
-router.post('/', isLoggedIn, boardController.createBoard);
-router.get('/', isLoggedIn, boardController.getBoardList);
-router.get('/:boardId', isLoggedIn, boardController.getBoard); // 로그인 확인 후 접근
-router.put('/:boardId', isLoggedIn, boardController.modifyBoard);
-router.delete('/:boardId', isLoggedIn, boardController.deleteBoard);
+router.post('/', authorizated, boardController.createBoard);
+router.get('/', authorizated, boardController.getBoardList);
+router.get('/:boardId', authorizated, boardController.getBoard); // 로그인 확인 후 접근
+router.put('/:boardId', authorizated, boardController.modifyBoard);
+router.delete('/:boardId', authorizated, boardController.deleteBoard);
+router.post(
+  '/:boardId/invitation',
+  authorizated,
+  boardController.inviteBoardGroupMember,
+);
+router.get(
+  '/:boardId/invitation',
+  authorizated,
+  boardController.getBoardGroupList,
+);
+router.patch(
+  '/:boardId/invitation',
+  authorizated,
+  boardController.modifyBoardGroupMemberPermission,
+);
+router.delete(
+  '/:boardId/invitation',
+  authorizated,
+  boardController.getBoardGroupList,
+);
 
 module.exports = router;
