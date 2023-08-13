b76ce980552ff2247f4b01aaa8a0b9d552b2fbca
Kim Joohee <syap1170@gmail.com>
2023-08-13 20:09:48 +0900

    feat: Add BoardGroup 캐싱데이터 관리
    데이터 수정될시 캐싱된 데이터도 수정되도록

--- cache.js
+++ cache.js
@@ -1,19 +1,69 @@
 const cache = require('./cacheInit');
-const { BoardGroup } = require('./models');
+const { BoardGroup, User } = require('./models');
 
 class CollaboratorCaching {
+  // 서버를 켰을때 BoardGroup 전체 데이터 캐싱
   setCachedCollaborators = async () => {
     try {
-      // cache.flushAll();
-
       const collaborators = await BoardGroup.findAll({});
 
-      for (const collaborator of collaborators) {
-        cache.set(`cacheKey${collaborator.collaborator}`, collaborator);
+      for (const row of collaborators) {
+        cache.set(`cacheKey${row.collaborator}`, row);
       }
       console.log('collaborator 데이터 캐싱 성공');
     } catch (err) {
       console.log('collaborator 데이터 캐싱 실패', err);
+      throw err;
+    }
+  };
+
+  // 초대한 멤버 데이터 캐싱
+  addCachedCollaborator = async (email) => {
+    try {
+      const inviteCollaborator = await BoardGroup.findOne({
+        include: { model: User, where: { email } },
+        raw: true,
+      });
+      cache.set(
+        `cacheKey${inviteCollaborator.collaborator}`,
+        inviteCollaborator,
+      );
+      console.log('collaborator 데이터 캐싱 추가 성공', inviteCollaborator);
+    } catch (err) {
+      console.log('collaborator 데이터 캐싱 추가 실패', err);
+      throw err;
+    }
+  };
+
+  // 초대된 멤버 퍼미션 수정시
+  modifyCachedCollaborator = async (userId) => {
+    try {
+      const modifyCachedCollaborator = await BoardGroup.findOne({
+        where: { collaborator: userId },
+      });
+
+      cache.set(
+        `cacheKey${modifyCachedCollaborator.collaborator}`,
+        modifyCachedCollaborator,
+      );
+      console.log('캐싱된 데이터 수정 성공', modifyCachedCollaborator);
+    } catch (err) {
+      console.log('캐싱된 데이터 수정 실패', err);
+      throw err;
+    }
+  };
+
+  deleteCachedCollaborator = async (userId) => {
+    try {
+      const cacheKey = `cacheKey${userId}`;
+      const collaborator = cache.get(cacheKey);
+
+      if (collaborator) {
+        cache.del(cacheKey);
+        console.log('캐싱 삭제 성공');
+      }
+    } catch (err) {
+      console.log('캐싱 삭제 실패', err);
       throw err;
     }
   };
--- services/board.service.js
+++ services/board.service.js
@@ -2,6 +2,9 @@ const MakeError = require('../utils/makeErrorUtil');
 const BoardRespotisoty = require('../repositories/board.repository');
 const BoardGroupRepository = require('../repositories/boardGroup.repository');
 const UserService = require('../services/user.service');
+const cache = require('../cacheInit');
+const CollaboratorCaching = require('../cache');
+
 const { Transaction } = require('sequelize');
 const { sequelize } = require('../models');
 
@@ -9,6 +12,7 @@ class BoardService {
   boardRepo = new BoardRespotisoty();
   boardGroupRepo = new BoardGroupRepository();
   userService = new UserService();
+  collaboratorCaching = new CollaboratorCaching();
 
   // 보드 생성
   createBoard = async (userId, name, color, description) => {
@@ -157,6 +161,8 @@ class BoardService {
       permission,
     );
 
+    await this.collaboratorCaching.addCachedCollaborator(email);
+
     if (!result) {
       throw new MakeError(400, '멤버 초대에 실패하였습니다.');
     }
@@ -180,7 +186,7 @@ class BoardService {
 
   // 보드 멤버 권한 수정
   modifyBoardGroupMemberPermission = async (boardId, userId, permission) => {
-    if (isNaN(boardId) || isNaN(userId) || permission) {
+    if (isNaN(boardId) || isNaN(userId) || !permission) {
       throw new MakeError(400, '잘못된 형식입니다.');
     }
 
@@ -189,6 +195,8 @@ class BoardService {
       userId,
       permission,
     );
+
+    await this.collaboratorCaching.modifyCachedCollaborator(userId);
 
     if (!result) {
       throw new MakeError(400, '수정이 실패하였습니다.');
@@ -207,6 +215,9 @@ class BoardService {
       boardId,
       userId,
     );
+
+    await this.collaboratorCaching.deleteCachedCollaborator(userId);
+
     if (!result) {
       throw new MakeError(400, '삭제에 실패하였습니다.');
     }
